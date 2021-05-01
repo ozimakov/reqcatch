0ffe280ca63a071661a3038dccdbec32a820f75b
Oleg Zimakov <oleg@zimakov.cloud>
2021-05-01 22:37:58 +0300

    Heroku configuration and http port binding via PORT env var

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-const port = 3000
+const port = process.env.PORT || 3000
 const max_messages = 100
 
 let messages = []
